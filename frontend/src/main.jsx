065ec1669193080801ecb8b86d44cad87df31141
shreyash jain <sheryash123jain@gmail.com>
2024-03-03 11:33:51 +0530

    u

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter} from "react-router-dom"
 // import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     {/* <App />
      */}
      <h1>hsjkdhsa</h1>
-  </React.StrictMode>,
+  </BrowserRouter>,
 )
